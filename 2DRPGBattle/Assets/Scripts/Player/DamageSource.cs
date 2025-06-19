e7e35666fc5a6fa7c221c4c397306c7c35d02b18
Kuro1604 <122670449+Kuro1604@users.noreply.github.com>
2025-06-19 19:44:11 +0700

    magic
    magic

--- 2DRPGBattle/Assets/Scripts/Player/DamageSource.cs
+++ 2DRPGBattle/Assets/Scripts/Player/DamageSource.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 
 public class DamageSource : MonoBehaviour
 {
-    [SerializeField] private int damageAmount = 1;
-   private void OnTriggerEnter2D(Collider2D other)
+    private int damageAmount;
+
+    private void Start()
+    {
+        MonoBehaviour currenActiveWeapon = ActiveWeapon.Instance.CurrentActiveWeapon;
+        damageAmount = (currenActiveWeapon as IWeapon).GetWeaponInfo().weaponDamage;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
     {
         EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
         enemyHealth?.TakeDamage(damageAmount);
--- 2DRPGBattle/Assets/Scripts/Staff.cs
+++ 2DRPGBattle/Assets/Scripts/Staff.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class Staff : MonoBehaviour, IWeapon
 {
-    [SerializeField] private WeaponInfo weaponInfo; 
+    [SerializeField] private WeaponInfo weaponInfo;
+    [SerializeField] private GameObject magicLaser;
+    [SerializeField] private Transform magicLaserSpawnPoint;
+
+    private Animator myAnimator;
+
+    readonly int AttackHash = Animator.StringToHash("Attack");
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
     private void Update()
     {
         MouseFollowWithOffset();
@@ -13,9 +24,18 @@ public class Staff : MonoBehaviour, IWeapon
 
     public void Attack()
     {
-        Debug.Log("Staff Attack");
+        myAnimator.SetTrigger(AttackHash);
     }
 
+    public void SpawnStaffProjectileAnimEvent()
+    {
+        GameObject newLaser = Instantiate(magicLaser, magicLaserSpawnPoint.position, Quaternion.identity);
+    }
+
+    public WeaponInfo GetWeaponInfo()
+    {
+        return weaponInfo;
+    }
     private void MouseFollowWithOffset()
     {
         Vector3 mousePos = Input.mousePosition;
@@ -31,10 +51,5 @@ public class Staff : MonoBehaviour, IWeapon
         {
             ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
         }
-    }
-
-    public WeaponInfo GetWeaponInfo()
-    {
-        return weaponInfo;
     }
 }

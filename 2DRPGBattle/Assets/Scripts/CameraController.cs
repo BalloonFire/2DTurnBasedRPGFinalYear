cc0e387c2b49e9a9e3659c4bd3e8cb4944274e2d
Kuro1604 <122670449+Kuro1604@users.noreply.github.com>
2025-06-23 12:33:07 +0700

    ghost
    enemy

--- 2DRPGBattle/Assets/Scripts/CameraController.cs
+++ 2DRPGBattle/Assets/Scripts/CameraController.cs
@@ -1,11 +1,16 @@
-using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : Singleton<CameraController>
 {
     private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private void Start()
+    {
+        SetPlayerCameraFollow();
+    }
 
     public void SetPlayerCameraFollow()
     {
